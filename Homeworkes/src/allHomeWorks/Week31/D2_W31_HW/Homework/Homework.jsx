c978c5ddbd031ddf081b1d28b3f3ee0ca18b60c9
weiliams596 <mhy596520@gmail.com>
2025-04-16 14:07:05 +0500

    Submit homework

--- Homeworkes/src/allHomeWorks/Week31/D2_W31_HW/Homework/Homework.jsx
+++ Homeworkes/src/allHomeWorks/Week31/D2_W31_HW/Homework/Homework.jsx
@@ -1,42 +1,59 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect, useReducer } from "react";
 
 const initialState = {
-  lastUser:''
+  lastUser: "",
+  isLoading:false
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'UPDATE_USER':
-      localStorage.setItem('lastUser',action.payload);
+    case "LOADING":
       return {
-       ...state,
-        lastUser: action.payload
+        ...state,
+        isLoading:true
+      }
+    case "UPDATE_USER":
+      localStorage.setItem("lastUser", action.payload);
+      return {
+        ...state,
+        lastUser: action.payload,
+        isLoading:false
       };
     default:
       return state;
   }
-}
-
+};
 
 export default function Homework() {
-  const [state,dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  useEffect(()=>{
-      const lastUser = localStorage.getItem('lastUser');
-      if(lastUser){
-        dispatch({type:'UPDATE_USER',payload:lastUser});
-      }
-  },[]);
+  useEffect(() => {
+    const lastUser = localStorage.getItem("lastUser");
+    if (lastUser) {
+      dispatch({ type: "UPDATE_USER", payload: lastUser });
+    }
+  }, []);
   return (
     <div>
       <form>
         <label>Insert new user:</label>
-        <input type="text"/>
-        <button type="submit" onClick={() => {
-          dispatch({type:'UPDATE_USER',payload:document.querySelector('input').value});
-        }}>Submit</button>
+        <input type="text" />
+        <button
+          type="submit"
+          onClick={() => {
+            dispatch({ type: "LOADING" });
+            setTimeout(() => {
+              dispatch({
+                type: "UPDATE_USER",
+                payload: document.querySelector("input").value,
+              });
+            }, 1000);
+          }}>
+          Submit
+        </button>
         <p>Last user: {state.lastUser}</p>
+        {state.isLoading && <p>Loading...</p>}
       </form>
     </div>
-  )
+  );
 }
